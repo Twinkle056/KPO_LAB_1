0ff039da1fbf19700f743ec0a8f0f04d8a486241
Pavel <pavlik010105@gmail.com>
2023-03-19 17:21:26 +0300

    Add File

---
+++ pythonProject/uni2.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+m = 0
+m = a+b+c
+if a*b+c > m:
+    m = a*b+c
+if a*(b+c) > m:
+    m = a*(b+c)
+if a+b*c > m:
+    m = a+b*c
+if (a+b)*c > m:
+    m = (a+b)*c
+if a*b*c > m:
+    m = a*b*c
+print(m)
